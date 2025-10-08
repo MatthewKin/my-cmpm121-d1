d35fb659cb7fe7af2d7099360664d2749e96c0a2
MatthewKin <166259185+MatthewKin@users.noreply.github.com>
2025-10-07 18:15:37 -0700

    Fix body overwrite, preserve button

--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,13 @@
 import exampleIconUrl from "./noun-paperclip-7598668-00449F.png";
 import "./style.css";
 
-document.body.innerHTML = `
-  <p>Example image asset: <img src="${exampleIconUrl}" class="icon" /></p>
+const pageContent = document.createElement("div");
+pageContent.innerHTML = `
+<p>Example image asset: <img src="${exampleIconUrl}" class="icon" /></p>
+<h1>I can see the text yippee</h1>"
 `;
+
+document.body.append(pageContent);
 
 const button = document.createElement("button");
 button.textContent = "Talk to The Website";
@@ -13,5 +17,4 @@ button.style.margin = "20px";
 button.addEventListener("click", () => {
   alert("HELLO USER! :DDD");
 });
-
 document.body.appendChild(button);
